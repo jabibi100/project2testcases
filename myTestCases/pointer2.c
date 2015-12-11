4fa77c825b6b5776e655db1b599831fcfc94e58d
Javid Habibi <jabibi100@gmail.com>
2015-12-10 20:35:13 -0500

    adding difficult pointer test

---
+++ myTestCases/pointer2.c
@@ -0,0 +1,28 @@
+int * c;
+
+foo(int * blah){
+	*blah = 10;
+}
+
+int main(){
+	int a;
+	int * b;
+
+	int var = 20;
+	int * p1,p2,p3;
+
+	if(var){
+		*p1 = 10;
+	}
+	else{
+		p2 = &var;
+	}
+
+	printf("%d %d\n", p1,p2);
+
+	foo(&a);
+	foo(b);
+	printf("%d %d\n" a, b);
+	foo1(*c);
+	printf("%d\n", c);	
+}
